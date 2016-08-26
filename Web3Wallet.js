849e095972fee73f9b3186bafaeb5079e6b6f27f
yuxuecai <yxc@blockchainlabs.com>
2016-08-26 13:58:58 +0800

    query  history transactions and

--- Web3Wallet.js
+++ Web3Wallet.js
@@ -15,7 +15,10 @@ function Web3Wallet(address, secret) {
 	
 	this.host;
 	this.web3 = new Web3();
-	
+	this.addressList = new Array();
+	this.transactions = new Array();
+	this.startBlockNo = 0;
+	// this.traversalBlocks(this.address);
 }
 
 Web3Wallet.prototype.initProvider = function(host) {
@@ -33,6 +36,175 @@ Web3Wallet.prototype.initProvider = function(host) {
 //	});
 	self.web3.setProvider(provider);
 }
+
+/**
+  listBalances:
+       function-----list all accounts' balances of some smart contract
+  param: 
+       contractAddress----- address of some smart contract
+  output:
+      console------<address  : balance  >
+  return:
+       null
+*/
+
+Web3Wallet.prototype.listBalances = function() {
+	var self = this;
+	
+	var PandaToken = self.web3.eth.contract(panda.abi);
+    
+	var ptInstance = PandaToken.at(self.address);
+	console.log(self.addressList);
+	self.addressList.forEach(function(e){
+	    console.log(e + " : " + self.web3.toBigNumber(ptInstance.balanceOf(self.toMapPos(e,3)))+ "           ");
+		console.log(e+":"+self.web3.toBigNumber(self.web3.eth.getStorageAt(self.address,self.toMapPos(e,3))));
+	});
+}
+
+Web3Wallet.prototype.toMapPos = function(address, mapPosIndex) {
+	var self = this;
+	var key = address.substring(2,address.length);
+	var zero24 = "000000000000000000000000";
+	var zero63 =zero24+zero24 +"000000000000000";
+	key = zero24+address + zero63+mapPosIndex;
+	var hashOfHash = self.web3.sha3(key, {encoding: 'hex'});
+	return hashOfHash;
+}
+
+/**
+  historyTransactions:
+       function-----list all history transactions of some smart contract
+  param: 
+       contractAddress----- address of some smart contract
+   output:
+       console----all transactions
+  return:
+       null
+*/
+
+Web3Wallet.prototype.historyTransactions = function() {
+	var self = this;
+	console.log("I am in historyTransactions");
+	var PandaToken = self.web3.eth.contract(panda.abi);
+
+	var ptInstance = PandaToken.at(self.address);
+	self.transactions.forEach(function(tx){
+		console.log(tx);
+	});
+}
+
+/**
+  traversalBlocks:
+       function-----ltraversal all blocks in the chain, list all history transactions of some smart contract and accounts
+  param: 
+       contractAddress----- address of some smart contract
+   output:
+       transactions []----all transactions
+	   addressList []---- all accounts
+  return:
+       null
+*/
+
+Web3Wallet.prototype.traversalBlocks = function(contractAddress) {
+	var self = this;
+	var blockNum = self.web3.eth.blockNumber;
+	var tTxNum = 0;
+	
+	var trlBadrs = contractAddress.toLowerCase();
+	
+	for (i=self.startBlockNo; i<blockNum; i++) {
+		var txNum = self.web3.eth.getBlockTransactionCount(i);
+		if (0===i%100) {
+			console.log(txNum + " " + i);
+		}
+		/*
+		var block = self.web3.eth.getBlock(i,true);
+		if (block != null && block.transactions != null) {
+           block.transactions.forEach( function(tx) {
+			   if (tx.to == trlBadrs ) {
+				  var txr =  self.web3.eth.getTransactionReceipt(tx.hash);
+				   self.transactions[tTxNum++] = tx; 
+				   var xlen = txr.from.length;
+				   var ylen = txr.logs[0].topics[1].length;
+				    var rAddress = "0x" + txr.logs[0].topics[2].substring(ylen-xlen+2, ylen);
+			      self.updateAddressList(rAddress);
+				  self.updateAddressList(tx.from);
+			   }
+		   } );
+		} */
+		
+		for(j=0; j<txNum; j++) {
+			
+			var tx = self.web3.eth.getTransactionFromBlock(i, j);
+			var txr = self.web3.eth.getTransactionReceipt(tx.hash);
+			
+			if (tx.to == trlBadrs ) {
+				
+				self.transactions[tTxNum++] = tx;
+				var xlen = txr.from.length;
+				var ylen = txr.logs[0].topics[1].length;
+				var rAddress = "0x" + txr.logs[0].topics[2].substring(ylen-xlen+2, ylen);
+				console.log(rAddress);
+			    self.updateAddressList(rAddress);
+				console.log(tx.from);
+				self.updateAddressList(tx.from);
+				
+			}
+		}
+	}
+	self.startBlockNo = blockNum;
+}
+
+/**
+  updateAddressList:
+       function-----insert an account of some smart contract
+  param: 
+       address----- address of an account using some smart contract
+   output:
+	   addressList []---- all accounts
+  return:
+       null
+*/
+
+Web3Wallet.prototype.updateAddressList = function(address) {
+	var self = this;
+	
+	var adrNum = self.addressList.length;
+	
+	
+	 
+	var pos = self.findAddress(address);
+	if (pos > adrNum-1) {
+		self.addressList.push(address);
+	} else {
+		self.addressList.splice(pos,address);
+	}
+	//.addressList = addressList;
+}
+
+/**
+  findAddress:
+       function-----look for the pos of a given address
+  param: 
+       address----- address of an account using some smart contract
+   output:
+	   addressList []---- all accounts
+  return:
+       null
+*/
+
+Web3Wallet.prototype.findAddress = function(address) {
+	var self = this;
+	var addressList = self.addressList;
+	var posi = 0;
+	addressList.some(function(e){
+		posi++; 
+		return e>address;
+	});
+	return posi;
+	
+}
+
 
 Web3Wallet.prototype.getBalance = function(address) {
 	var self = this;
--- test/Web3WalletTest.js
+++ test/Web3WalletTest.js
@@ -12,7 +12,7 @@ console.log('getSeq', wallet.getSeq());
 
 console.log('getGasPrice', wallet.getGasPrice());
 
-wallet.transferPanda('0x1552f2d8C79CCEe276dfD399229f6985383926a4', 288, function(err, hash) {
+wallet.transferPanda('0x38bf9c59e8129fa13f19cbeecca5131b52f6ab04', 28, function(err, hash) {
 	if (err) {
 		console.error('Transfer Fail!');
 		console.error(err);
@@ -20,3 +20,8 @@ wallet.transferPanda('0x1552f2d8C79CCEe276dfD399229f6985383926a4', 288, function
 		console.log('Done with HASH:', hash);
 	}
 });
+
+
+wallet.traversalBlocks("0xA494B85566a591B246B481039aD06eeA1a9CDc90");
+wallet.listBalances();
+ wallet.historyTransactions();
